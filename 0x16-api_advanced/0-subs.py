d7b9f21b024fac177afebf473c5ee8a3a7c1b660
DouaaAl <elmahraouidouaa123@gmail.com>
2024-08-14 06:08:50 +0000

    0.0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" script to obtain subscribers
-    count from a subreddit
 """
+A script to fetch the number of subscribers for a given subreddit using the Reddit API.
+"""
+
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """ function to get subscriber count"""
-    if subreddit and type(subreddit) is str:
+    """Function to get the subscriber count from a subreddit."""
+    if subreddit and isinstance(subreddit, str):
         subscribers = 0
         url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
+        headers = {'User-Agent': 'my-app/0.0.1'}
         req = get(url, headers=headers)
         if req.status_code == 200:
             data = req.json()
