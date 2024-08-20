d97e11df1744bbc7d453f06745c1aa693c95c8e3
DouaaAl <elmahraouidouaa123@gmail.com>
2024-08-20 20:07:09 +0000

    A.0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# let's create a file in /tmp using puppet
-
-file { 'school':
+# ensuring its a file
+file { '/tmp/school':
+  ensure  => file,
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
+  content => 'I love Puppet'
 }
