eb0c3d619378260059e788c1dd2fad3bdda928c7
DouaaAl <elmahraouidouaa123@gmail.com>
2024-08-20 20:33:06 +0000

    A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,10 @@
-# Ensure pip3 is installed (only if necessary)
-package { 'python3-pip':
+# Ensure Python and pip are installed
+class { 'python':
+  pip    => true,
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 using pip3
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  notify  => Exec['verify-flask'],
-}
-
-# Optionally verify installation
-exec { 'verify-flask':
-  command => '/usr/bin/pip3 show Flask',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  refreshonly => true,
+# Install Flask version 2.1.0
+python::pip { 'Flask':
+  ensure => '2.1.0',
 }
